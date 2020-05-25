3edadcd47ca649c68ca472f76c3f238ea624ea66
Tony Silvestri <silvestri@stcc.edu>
2020-05-25 19:58:47 -0400

    Added LeaveTypes Repositories and CRUD, DataTables, and FontAwesome

--- leave-management/Data/ApplicationDbContext.cs
+++ leave-management/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using leave_management.Models;
 
 namespace leave_management.Data
 {
@@ -20,6 +21,8 @@ namespace leave_management.Data
         public DbSet<LeaveType> LeaveTypes { get; set; }
 
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        public DbSet<leave_management.Models.LeaveTypeVM> DetailsLeaveTypeVM { get; set; }
 
     }
 }
